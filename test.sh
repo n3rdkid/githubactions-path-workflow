e9fa4e73b936caa654a77667e65fe01d6a0c1080
Saurav Adhikari <sauravads123@gmail.com>
2021-07-21 17:42:42 +0545

    done

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
-if grep -q "refs/tags/v" <<< "refs/tags/v0.0.3"; then
-  echo "::set-output name=env::PROD"
-else
-  echo "::set-output name=env::DEV"
+old_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-count=1))
+new_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
+admin_diff_list=$(git diff $old_tag $new_tag --name-only | grep packages/consumer)
+if [[ ${#admin_diff_list} -ne 0 ]]; then
+  echo "::set-output name=is_admin::true"
+  echo "TRUE"
+else 
+  echo "False"
 fi
