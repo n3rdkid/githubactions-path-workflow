20a1e0a6f901dcfbb7000239c298b2115c3e505d
Saurav Adhikari <sauravads123@gmail.com>
2021-07-21 21:06:57 +0545

    minor: test

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 old_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-count=1))
+echo $old_tag
 new_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
-admin_diff_list=$(git diff $old_tag $new_tag --name-only | grep packages/consumer)
-if [[ ${#admin_diff_list} -ne 0 ]]; then
-  echo "::set-output name=is_admin::true"
-  echo "TRUE"
-else 
-  echo "False"
-fi
+echo $new_tag
+git diff $old_tag $new_tag --name-only
