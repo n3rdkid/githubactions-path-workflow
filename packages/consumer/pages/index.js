7bba69c35bcc926433c734824c47ecc4be0afca7
Saurav <sauravads123@gmail.com>
2021-07-23 17:06:56 +0545

    test

--- packages/consumer/pages/index.js
+++ packages/consumer/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Consumer App!!!
+          MOSHI MOSHI
         </h1>
        </main>
     </div>
