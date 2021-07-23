3c6cfe26cdca35d447b5452a403fbe72312a646b
Saurav <sauravads123@gmail.com>
2021-07-23 17:51:49 +0545

    test PR trigger

--- packages/consumer/pages/_app.js
+++ packages/consumer/pages/_app.js
@@ -2,21 +2,7 @@ import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }) {
-
-
-// One
-// Onethos 
-// yellow
-// One
-// One
-// One
-// One
-// One
-// One
-
-
-
-  
+ 
   return <Component {...pageProps} />
 }
 
