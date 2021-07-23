7baa1f7e95c3b0510279739c9f7ce3189d811294
Saurav <sauravads123@gmail.com>
2021-07-23 17:07:23 +0545

    SAYONARA

--- packages/consumer/pages/index.js
+++ packages/consumer/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Consumer App!!!
+          SAYONARA 
         </h1>
        </main>
     </div>
